8cc779341b00289e9dc3cf52508ad7883a495da1
Necolanch <NICruz@student.fullsail.edu>
2022-09-08 13:51:16 -0400

    Setup & migrated model and established routes

---
+++ migrations/20220908150702-create-spotify-token.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('SpotifyTokens', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      accessToken: {
+        type: Sequelize.STRING
+      },
+      tokenType: {
+        type: Sequelize.STRING
+      },
+      expiresIn: {
+        type: Sequelize.BIGINT
+      },
+      refreshToken: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('SpotifyTokens');
+  }
+};
--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const spotify=require("./routes/spotify");
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(cors());
+
+app.use("/spotify/v1", spotify);
 
 app.listen(process.env.PORT || 3001);
